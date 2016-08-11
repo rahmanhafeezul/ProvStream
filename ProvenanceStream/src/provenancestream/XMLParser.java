0e786a0b4a3378ac2ef36612dd0ed34466e8044f
rahmanhafeezul <rahman.hafeezul@gmail.com>
2016-08-11 21:24:38 +0530

    XML parser

--- ProvenanceStream/src/provenancestream/XMLParser.java
+++ ProvenanceStream/src/provenancestream/XMLParser.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Script to conditions specified in constraints.xml
  */
 package provenancestream;
 import java.io.IOException;
