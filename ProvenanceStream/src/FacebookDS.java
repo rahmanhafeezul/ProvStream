9e5558e93c1f92083f426aa0e78699cc083dc3e2
rahmanhafeezul <rahman.hafeezul@gmail.com>
2016-08-11 21:22:29 +0530

    Facebook Dataset visualize

--- ProvenanceStream/src/FacebookDS.java
+++ ProvenanceStream/src/FacebookDS.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Code to load facebook dataset and visualize it using GraphStream library
  */
 import java.io.BufferedReader;
 import java.io.File;
