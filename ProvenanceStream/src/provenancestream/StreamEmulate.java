a3abe540a63152b74e040228c8f317bfdef1eba7
rahmanhafeezul <rahman.hafeezul@gmail.com>
2016-08-11 21:11:48 +0530

    Render a streaming provenance graph

--- ProvenanceStream/src/provenancestream/StreamEmulate.java
+++ ProvenanceStream/src/provenancestream/StreamEmulate.java
@@ -4,8 +4,7 @@
  * and open the template in the editor.
  */
 package provenancestream;
-import java.io.IOException;
-import org.graphstream.graph.Graph;
+import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
@@ -18,23 +17,18 @@ public class StreamEmulate {
 		String filePath = "/home/rahman/NetBeansProjects/rahman-project/ProvenanceStream/sample_prov.dgs";
 		Graph g = new DefaultGraph("ProvStream");
 		FileSource fs = FileSourceFactory.sourceFor(filePath);
-                g.display();
-		fs.addSink(g);
-
-		try {
-			fs.begin(filePath);
-
-			while (fs.nextEvents()) {
-				   Thread.sleep(10000);
+                fs.addSink(g);
+                g.addAttribute("ui.stylesheet", "node { text-mode: normal; text-style: bold; fill-color: blue;}");
+                g.display();                
+		fs.begin(filePath);
+		while (fs.nextEvents()) {
+                        for (Node node : g) {
+                                node.addAttribute("ui.label", node.getId());
+                                }
+				Thread.sleep(2000);
 			}
-		} catch( IOException e) {
-		}
-
-		try {
-			fs.end();
-		} catch( IOException e) {
-		} finally {
-			fs.removeSink(g);
-		}
+                fs.end();
+		fs.removeSink(g);
+		
 	}
 }
