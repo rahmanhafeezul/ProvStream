e9009d9c9745c827aaf4677615a3d7ecedbb0cc4
rahmanhafeezul <rahman.hafeezul@gmail.com>
2016-08-25 21:30:08 +0530

    provModel

---
+++ ProvenanceStream/src/provenancestream/algorithm/ShortestPath.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provenancestream.algorithm;
+
+/**
+ *
+ * @author rahman
+ */
+public class ShortestPath {
+    /**
+     * Get the shortest path between two nodes in the provGraph or an empty graph if no path
+     * exists
+     * 
+     * @param pdg
+     *            Graph to find a path in
+     * @param sources
+     *            possible first nodes in the path
+     * @param dests
+     *            possible last nodes in the path
+     * @return A graph consisting of a shortest path between the two nodes or empty if none exists
+     */
+}
+
